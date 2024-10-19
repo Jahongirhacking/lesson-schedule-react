dff4886172b41b26f25f85435562d9b24472853c
Jahongirhacking <Jahongirhacking@gmail.com>
2024-10-19 19:20:18 +0500

    feat: export important variables to publish

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './components/Schedule/Schedule.module.scss';
+import './components/Schedule/style.scss';
+import './components/TimeTable/style.scss';
+export { default as TimeTable } from './components/TimeTable/index';
+export { default as makeTimeTable } from './components/TimeTable/makeTimeTable';
+export * from './types/index';
